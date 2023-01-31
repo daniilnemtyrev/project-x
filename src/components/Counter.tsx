d62a141d25ceec591f63bd4e2d2cf0fa1f9a9487
daniilnemtyrev <cool.dent2012@yandex.ru>
2023-01-31 16:01:22 +0400

    init

---
+++ src/components/Counter.tsx
@@ -0,0 +1,9 @@
+import classes from "./Counter.module.scss";
+
+export const Counter = () => {
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>TEST</h1>
+    </div>
+  );
+};
