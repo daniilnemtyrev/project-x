b3ccd02492ab7a6916e207dc046ff5815fc088eb
daniilnemtyrev <cool.dent2012@yandex.ru>
2023-03-11 17:57:52 +0400

    feat(loki): new screens

--- src/shared/config/storybook/decorators/StoreDecorator.tsx
+++ src/shared/config/storybook/decorators/StoreDecorator.tsx
@@ -2,10 +2,12 @@ import { DeepPartial } from '@reduxjs/toolkit'
 import { Story } from '@storybook/react'
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 import { DeepPartialReducers } from 'app/providers/StoreProvider/config/StateSchema'
+import { profileReducer } from 'entities/Profile'
 import { loginReducer } from 'features/AuthByUsername'
 
 const defaultAsyncReducers: DeepPartialReducers = {
     login: loginReducer,
+    profile: profileReducer,
 }
 
 export const StoreDecorator =
